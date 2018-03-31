db9fa9245b05dfbfa48c5ed67a7d4139e1b4bca5
Andres Cuervo <andrescuervor@gmail.com>
2018-03-31 15:27:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+require('aframe')
+require('aframe-animation-component')
+require('aframe-extras.ocean')
+require('aframe-gradient-sky')
+require('aframe-particle-system-component')
+
+console.log("hello world");
